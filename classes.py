951fff3b31a2b945504a06e99192663955731972
Patrick Dawkins <dawkinspw@gmail.com>
2021-11-19 08:07:00 -0800

    init commit

---
+++ classes.py
@@ -0,0 +1,22 @@
+class Circle:
+    Id = 0
+    radius = 0
+    color = ""
+    display = True
+
+    def __init__(self,id,r,c,d):
+        self.Id = id
+        self.radius = r
+        self.color = c
+        self.display = d
+
+    def circumfrence(self):
+         return 3.141 * 2 * self.radius
+
+circle1 = Circle(1, 10, "Blue", True)
+circle2 = Circle(2, 8, "Yellow", True)
+circle3 = Circle(3, 17, "Red", False)
+
+print("Circle 1 circumfrence is: " + str(circle1.circumfrence()))
+print("Circle 2 circumfrence is: " + str(circle2.circumfrence()))
+print("Circle 3 circumfrence is: " + str(circle3.circumfrence()))
