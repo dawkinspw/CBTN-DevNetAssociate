35ab04afd4ae662b28f71c824ed4cc91f6dc19d5
Patrick Dawkins <dawkinspw@gmail.com>
2021-11-26 08:29:30 -0800

    Commenting code

--- Data_Structures/xml_sample.py
+++ Data_Structures/xml_sample.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 import os
 
+# User realpath to reliably use the file in the same directory as the script
+# Source: https://stackoverflow.com/a/4060259
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
--- Data_Structures/xml_sample2.py
+++ Data_Structures/xml_sample2.py
@@ -1,6 +1,8 @@
 from lxml import etree as ET
 import os
 
+# User realpath to reliably use the file in the same directory as the script
+# Source: https://stackoverflow.com/a/4060259
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
