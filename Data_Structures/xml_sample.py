803e9bab16ed3d10dd431660855bb91d6da58826
Patrick Dawkins <dawkinspw@gmail.com>
2021-11-26 08:20:10 -0800

    Add realpath  function to reliably call local file

---
+++ Data_Structures/xml_sample.py
@@ -0,0 +1,23 @@
+import xml.etree.ElementTree as ET
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# Get the XML file data
+stream = open(os.path.join(__location__, 'sample.xml'), 'r')
+
+# Parse the data into an ElementTree object
+xml = ET.parse(stream)
+
+# Get the 'root' Element object from the ElementTree
+root = xml.getroot()
+
+# Iterate through each child of the root Element
+for e in root:
+    # Print the stringified version of the element
+    print(ET.tostring(e))
+    print("")
+
+    # Print the "Id" attribute of the Element object
+    print(e.get("Id"))
