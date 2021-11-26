8df1eee09a8e6b31604bbb1bce3d37ad14c0f603
Patrick Dawkins <dawkinspw@gmail.com>
2021-11-26 08:32:04 -0800

    xmltodict example

---
+++ Data_Structures/xml_sample3.py
@@ -0,0 +1,16 @@
+import xmltodict
+import os
+
+# User realpath to reliably use the file in the same directory as the script
+# Source: https://stackoverflow.com/a/4060259
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# Get the XML file data
+stream = open(os.path.join(__location__, 'sample.xml'), 'r')
+
+# Parse the XML file into an 'OrderedDic'
+xml = xmltodict.parse(stream.read())
+
+for e in xml:
+    print(e)
