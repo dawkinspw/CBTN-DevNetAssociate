5173c057ffbcc0c82be69257e6d8ace4bfacef69
Patrick Dawkins <dawkinspw@gmail.com>
2021-11-26 19:48:43 -0500

    Added for loop

--- Data_Structures/yaml_sample.py
+++ Data_Structures/yaml_sample.py
@@ -6,3 +6,11 @@ from yaml import load, load_all
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
+stream = open(os.path.join(__location__, 'sample.yaml'))
+documents = load_all(stream, Loader=yaml.FullLoader)
+
+print(type(documents))
+
+for doc in documents:
+    print(type(doc))
+    print(doc['people'][1]['FirstName'])
