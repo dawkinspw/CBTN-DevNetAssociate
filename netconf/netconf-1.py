24ec552521c546899c1a915179a5652eea9b80f4
Patrick Dawkins <dawkinspw@gmail.com>
2021-12-28 11:00:44 -0800

    Initial netconf script

---
+++ netconf/netconf-1.py
@@ -0,0 +1,9 @@
+from ncclient import manager
+
+router = {"host": "10.10.20.48", "port": "830", 
+            "username": "developer", "password": "C1sco12345"}
+
+with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
+    for capability in m.server_capabilities:
+        print('*' * 50)
+        print(capability)
